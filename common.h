64a7d8b91aa8949f9c764fdbaefa8a5a03b85e3a
Nailuj29 Gaming <nailuj29@nailuj29gaming@gmail.com>
2021-05-31 12:25:02 -0400

    Initial commit

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#define DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT_CODE
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#endif
---
+++ compiler.h
@@ -0,0 +1,9 @@
+#ifndef CLOX_COMPILER_H
+#define CLOX_COMPILER_H
+
+#include "vm.h"
+#include "object.h"
+
+bool compile(const char* source, Chunk* chunk);
+
+#endif
